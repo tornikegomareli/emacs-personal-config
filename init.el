9326fa5845ab521e0c730a0dc208549a8762b188
Tornike Gomareli <gomarelidevelopment@gmail.com>
2023-05-06 20:58:14 +0400

    initial emacs configuration commit

---
+++ init.el
@@ -0,0 +1,91 @@
+(setq inhibit-startup-message t) ;; Remove startup message
+
+(scroll-bar-mode -1) ; Disable visible scrollbar
+(tool-bar-mode -1) ; Disalbe the toolbar
+(tooltip-mode -1) ; Disable tooltips
+(set-fringe-mode 10) ; Give some breathing room
+(menu-bar-mode -1) ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;; Make ESCc qquit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Font Fira code size 200 because of DP
+(set-face-attribute 'default nil :font "Fira Code" :height 200 )
+
+;; Load theme 
+(load-theme 'wombat)
+
+;; Make Full screen when open Emacs
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;;
+
+;; Mapping macOS keybindings to emacs C,M
+(setq mac-control-modifier 'control)
+(setq mac-right-option-modifier 'control)
+
+
+;; Reload Emacs configuration
+(defun reload-init-file ()
+  (interactive)
+  (load-file "~/.emacs"))
+
+
+;; Reload file with Control c r
+(global-set-key (kbd "C-c r") 'reload-init-file)
+
+
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Counsel setup
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+  ("C-x b" . counsel-ibuffer)
+	("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start seaches with x
+
+;; Ivy - Auto completion for Emacs and Swiper which is file Search engine
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+ 
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
